ab351b5ff2a1eb0a0c2f3a5fe76b860a618f4c6c
Andrew Carroll <andrewc411@yahoo.com>
2022-04-27 14:26:39 -0400

    created emoji div and added styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
 const SELECTIONS = [
     {
         name: 'rock',
@@ -21,10 +22,33 @@ selectionButtons.forEach(selectionButton =>{
     selectionButton.addEventListener('click' , e =>{
         const selectionName = selectionButton.dataset.selection
         const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selectionName)
-        console.log(selection)
+        makeSelection(selection)
     })
 })
 
 function makeSelection(selection){
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
